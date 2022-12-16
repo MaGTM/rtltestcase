be873b9932964f4e12226e2189f96192df3d1291
MaGTM <uporoff.maxim@yandex.ru>
2022-12-16 18:54:41 +0600

    add base component styling | add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="main-content">
+    <sidebar class="base-box"></sidebar>
+    <div id="inventory" class="base-box">
+
+    </div>
+    <footer id="footer" class="base-box">
+
+    </footer>
+  </div>
 </template>
 
+<script>
+import Sidebar from "@/components/Sidebar";
+import Inventory from "@/components/Inventory";
+import Footer from "@/components/Footer";
+
+export default {
+  components: {
+    Sidebar,
+    Inventory,
+    Footer
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
+  body {
+    background-color: #303030;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  #main-content {
+    width: 785px;
+    height: 596px;
+    display: grid;
+    grid-template-columns: 236px 1fr 1fr;
+    grid-template-rows: 1fr 1fr 72px;
+    gap: 24px;
+    grid-template-areas:
+    "sidebar content content"
+    "sidebar content content"
+    "footer footer footer";
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .base-box {
+    background-color: #262626;
+    border: #4D4D4D 1px solid;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .skeleton-text {
+    background: linear-gradient(90deg, #3C3C3C 0%, #444444 51.04%, #333333 100%);
+  }
+
+  .skeleton-text.title {
+    height: 26px;
+  }
+  .skeleton-text.text {
+    height: 10px;
+  }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="sidebar">
+    <img :src="require('@/assets/images/Img Blur.png')" alt="sidebarimg">
+    <div class="sidebar-description">
+      <span class="skeleton-text title sidebar-description__title"></span>
+      <div class="sidebar-description__list">
+        <span
+            v-for="n in 6"
+            class="skeleton-text text sidebar-description__list__item"
+        >
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+#sidebar {
+  grid-area: sidebar;
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px 14px;
+}
+
+img {
+  width: 100%;
+}
+
+span {
+  display: block;
+  border-radius: 8px;
+}
+
+.sidebar-description {
+  width: 80%;
+  margin-top: 20px;
+}
+
+.sidebar-description__title {
+  margin-bottom: 24px;
+}
+
+.sidebar-description__list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  row-gap: 16px;
+}
+
+.sidebar-description__list__item:nth-child(1) {
+  width: 155px;
+}
+.sidebar-description__list__item:nth-child(2) {
+  width: 190px;
+}
+.sidebar-description__list__item:nth-child(3) {
+  width: 170px;
+}
+.sidebar-description__list__item:nth-child(4) {
+  width: 160px;
+}
+.sidebar-description__list__item:nth-child(5) {
+  width: 140px;
+}
+.sidebar-description__list__item:nth-child(6) {
+  margin-top: 8px;
+  width: 80px;
+}
+
+</style>
